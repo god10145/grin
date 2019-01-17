d90c1544dd82e2a36c9fc3ac91ab6b75370e2eac
god10145 <38486363+god10145@users.noreply.github.com>
2019-01-16 19:01:03 -0600

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir grin
+cd grin
+wget https://github.com/mimblewimble/grin-miner/releases/download/v1.0.1/grin-miner-v1.0.1-480578774-linux-amd64.tgz
+tar -xvzf grin-miner-v1.0.1-480578774-linux-amd64.tgz
+rm grin-miner-v1.0.1-480578774-linux-amd64.tgz
