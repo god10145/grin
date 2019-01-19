c651a741a6e063adba20dcfa8b05881fc37c98f8
god10145 <38486363+god10145@users.noreply.github.com>
2019-01-19 10:12:11 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,6 +2,21 @@
 
 mkdir grin
 cd grin
-wget https://github.com/mimblewimble/grin-miner/releases/download/v1.0.1/grin-miner-v1.0.1-480578774-linux-amd64.tgz
-tar -xvzf grin-miner-v1.0.1-480578774-linux-amd64.tgz
-rm grin-miner-v1.0.1-480578774-linux-amd64.tgz
+wget https://github.com/mimblewimble/grin-miner/releases/download/v1.0.2/grin-miner-v1.0.2-480780314-linux-amd64.tgz
+tar -xvzf grin-miner-v1.0.2-480780314-linux-amd64.tgz
+rm grin-miner-v1.0.2-480780314-linux-amd64.tgz
+cd grin-miner-v1.0.2
+mv * ..
+cd ..
+rm grin-miner-v1.0.2
+rm grin-miner.toml
+wget https://raw.githubusercontent.com/god10145/grin/master/grin-miner.toml
+sed -i "s/VAST/$VAST_CONTAINERLABEL/g" "grin-miner.toml"
+cd ~
+rm onstart.sh
+touch onstart.sh
+apt-get -y install screen
+touch /root/onstart.sh
+echo '#!/bin/bash
+screen -dmS miner /root/grin/grin-miner' > /root/onstart.sh
+chmod +x onstart.sh
